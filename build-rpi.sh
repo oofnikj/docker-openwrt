db7833f6f5915386725e843e0df2deb5bbdbddf0
Jordan Sokolic <oofnik@gmail.com>
2020-04-16 11:04:57 +0300

    notes

--- build-rpi.sh
+++ build-rpi.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
+# Extracts the rootfs from OpenWRT Raspberry Pi image available from
+# https://downloads.openwrt.org/releases/19.07.2/targets/brcm2708/bcm2708/
+# and builds a Docker container out of it
+#
+# Refer to https://openwrt.org/toh/raspberry_pi_foundation/raspberry_pi
+# to choose the right image
+#
+# If building on x86, you must have qemu-arm and binfmt-support installed
 set -e
 
 source openwrt.conf
